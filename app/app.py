b18e31f34cc71b79b5b15123491ec33ffa96485a
Alberto <albertiae.digital@gmail.com>
2022-12-20 20:48:24 -0400

    Created the function to read the tasks

--- app/app.py
+++ app/app.py
@@ -7,3 +7,33 @@ async def root() -> dict:
     return{
     "Hello":"World"
     }
+
+#@get (read)
+@app.get('/get',tags=['GET'])
+async def get_todo() -> dict:
+    return{'data':tasks}
+
+tasks = [
+    {
+    'ID':'01',
+    'Task':'Wakeup at 07:00 AM'
+    },
+    {
+    'ID':'02',
+    'Task':'Exercise in the morning'
+    },
+    {
+    'ID':'03',
+    'Task':'Get ready for work'
+    },
+    {
+    'ID':'04',
+    'Task':'Go to your first meeting today'
+    }
+]
+
+#@post (create)
+
+#@put (update)
+
+#@delete (delete)
